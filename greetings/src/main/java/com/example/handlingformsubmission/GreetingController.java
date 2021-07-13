064a9aaf30e178e3bae0de61f0af0d41d7afa87f
s.miyamoto <s.miyamoto@yourgames.co.jp>
2021-07-13 17:10:43 +0900

    add new file

---
+++ greetings/src/main/java/com/example/handlingformsubmission/GreetingController.java
@@ -0,0 +1,36 @@
+package com.example.handlingformsubmission;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GreetingController {
+
+	@Autowired
+	private DynamoDBEnhanced dde;
+
+	@Autowired
+	private PublishTextSMS msg;
+
+	@GetMapping("/")
+	public String greetingForm(Model model) {
+		model.addAttribute("greeting", new Greeting());
+		return "greeting";
+	}
+
+	@PostMapping("/greeting")
+	public String greetingSubmit(@ModelAttribute Greeting greeting) {
+
+		// Persist submitted data into a DynamoDB table using the enhanced client
+		dde.injectDynamoItem(greeting);
+
+		// Send a mobile notification
+		msg.sendMessage(greeting.getId());
+
+		return "result";
+	}
+}
